871597d611db7b372905e0ffbf5e80103107d6ed
InnerChoseEvgenia <evgenialukashen@gmail.com>
2024-12-08 21:02:54 +0300

    Add Fluent Validation, Global Handling Exceptions, Logging Behaviour, started Seeding CatalogDb, but didn't  finish

--- src/Services/Catalog/CatalogAPI/GlobalUsing.cs
+++ src/Services/Catalog/CatalogAPI/GlobalUsing.cs
@@ -2,6 +2,12 @@
 global using Mapster;
 global using MediatR;
 global using Marten;
+global using FluentValidation;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
 global using CatalogAPI.Models;
 global using CatalogAPI.Exceptions;
+global using CatalogAPI.Data;
+
