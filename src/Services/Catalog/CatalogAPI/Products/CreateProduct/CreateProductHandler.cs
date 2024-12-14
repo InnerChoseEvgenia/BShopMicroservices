126a37b61e1d70901b00763ada3360a085f3aaf1
InnerChoseEvgenia <evgenialukashen@gmail.com>
2024-12-14 21:00:06 +0300

    Add Seeding CatalogDB, Loggin Behavior, Global Handling Exception, Fluent Validation

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,4 @@
-﻿using FluentValidation;
-
+﻿
 namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline.
 app.MapCarter();
 app.UseExceptionHandler(options => { });
 
