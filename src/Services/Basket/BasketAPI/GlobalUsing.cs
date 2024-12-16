f8a91a2d3b18d566bff5e8efbc9dd89801690e77
InnerChoseEvgenia <evgenialukashen@gmail.com>
2024-12-16 17:05:37 +0300

    Add BasketAPI

---
+++ src/Services/Basket/BasketAPI/GlobalUsing.cs
@@ -0,0 +1,12 @@
+﻿global using BasketAPI.Models;
+global using BuildingBlocks.CQRS;
+global using FluentValidation;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using BuildingBlocks.Behaviors;
+global using Marten;
+global using BasketAPI.Data;
+global using BuildingBlocks.Exceptions;
+global using BasketAPI.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
