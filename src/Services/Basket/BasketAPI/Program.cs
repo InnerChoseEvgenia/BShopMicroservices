a3d496a4a6918798d84d0448b5def3b9acd12ef8
InnerChoseEvgenia <evgenialukashen@gmail.com>
2024-12-21 22:21:45 +0300

    Add DiscountGrpc, Consuming DiscountGrpc from BasketApi

--- src/Services/Basket/BasketAPI/Program.cs
+++ src/Services/Basket/BasketAPI/Program.cs
@@ -1,5 +1,9 @@
+using Discount.Grpc;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+//Application Services
 var assembly = typeof(Program).Assembly;
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
@@ -9,6 +13,8 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
+
+//Data Services
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
@@ -26,6 +32,24 @@ builder.Services.AddStackExchangeRedisCache(options =>
     //options.InstanceName = "Basket";
 });
 
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
+});
+
+
+//Cross-Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddHealthChecks()
@@ -34,6 +58,8 @@ builder.Services.AddHealthChecks()
 
 var app = builder.Build();
 
+
+// Configure the HTTP request pipeline.
 app.MapCarter();
 app.UseExceptionHandler(options => { });
 app.UseHealthChecks("/health",
